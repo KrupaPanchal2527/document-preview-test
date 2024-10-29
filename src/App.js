e9ff73a916cef7aa1f656031f4c9c56cd5336f7d
Krupa Panchal <krupapanchal.dev@gmail.com>
2024-10-29 16:29:18 +0530

    new feature

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
 function App() {
+  const [file, setFile] = React.useState(null);
+  const [url, setURL] = React.useState(null);
+
+  const handleChange = (e) => {
+    const file = e.target.files[0];
+    const blob = new Blob([file], { type: file.type });
+    setFile(blob);
+
+    const url = URL.createObjectURL(blob);
+    setURL(url);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <iframe
+        src="https://docs.google.com/gview?url=https://css4.pub/2015/icelandic/dictionary.pdf&embedded=true"
+        width="600px"
+        height="600px"
+        frameborder="1"
+        title="test"
+      ></iframe> */}
+
+      <h2>Section custom</h2>
+      <input type="file" onChange={handleChange} />
+     {url && (<>
+     <p>{url}</p>
+     <iframe
+        src={`https://docs.google.com/gview?url=${url}&embedded=true`}
+        width="600px"
+        height="600px"
+        frameborder="1"
+      /></>)}
     </div>
   );
 }
